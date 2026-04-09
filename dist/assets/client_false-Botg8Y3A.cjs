8fea5a7e9ee66aee011f45dffcf3488a5065f428
W-P-DPP <1519037604@qq.com>
2026-04-09 09:57:28 +0800

    add

---
+++ dist/assets/client_false-Botg8Y3A.cjs
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
+const client_false = {};
+throw new Error(`Could not resolve "@node-rs/xxhash" imported by "@redis/client".`);
+exports.default = client_false;
